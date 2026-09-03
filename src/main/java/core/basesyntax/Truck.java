81be5e826870768ff189e7274e71d4f27e215ef5
wstempkowicz <80699308+wstempkowicz@users.noreply.github.com>
2026-09-03 22:46:53 +0200

    task

---
+++ src/main/java/core/basesyntax/Truck.java
@@ -0,0 +1,10 @@
+package core.basesyntax;
+
+public class Truck extends Machine {
+    public void doWork(){
+        System.out.println("Truck start to work");
+    }
+    public void stopWork(){
+        System.out.println("Truck stopped to work");
+    }
+}
